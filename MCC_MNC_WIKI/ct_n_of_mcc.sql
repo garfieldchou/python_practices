ace96a398c39f33feac7a65e7a2a0e6430ab1aa0
shaudai <shaudai0402@hotmail.com>
2016-11-25 21:39:02 +0800

    [SQL] Output country name instead of country id

--- MCC_MNC_WIKI/ct_n_of_mcc.sql
+++ MCC_MNC_WIKI/ct_n_of_mcc.sql
@@ -1,4 +1,6 @@
-select country_id, count(distinct mcc)
-from mcc_mnc
+select country.name, count(distinct mcc) as counts
+from mcc_mnc join country
+on country.id = mcc_mnc.country_id
 group by country_id
 having count(distinct mcc) > 1
+order by counts desc
